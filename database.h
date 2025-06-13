f29f62255446486a7874354e06d7dd2467c593b4
lledezma <lledezma>
2025-06-13 15:56:27 -0500

    Add methods and attributes

--- database.h
+++ database.h
@@ -5,18 +5,24 @@
 #include <QSqlDatabase>
 #include <QSqlQuery>
 #include <QSqlRecord>
+#include <QSqlError>
 
 class Database: public QObject
 {
     Q_OBJECT
 public:
-    explicit Database(QString database, QString dbName, QObject *parent = nullptr);
+    explicit Database(QString database, QString dbName, QString table, QObject *parent = nullptr);
     ~Database();
-    void selectAll(QString table);
+    void selectAll();
+    void dropRecord(QString column, QString value);
+    void addRecord(QStringList values);
 
 private:
     QSqlDatabase db;
     QSqlRecord row;
+    QString table;
+
+
 
 };
 
